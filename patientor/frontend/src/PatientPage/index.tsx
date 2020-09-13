52e985e6bd2821923df161c063713b03d011cf09
arskdj <68353557+arskdj@users.noreply.github.com>
2020-09-13 20:51:59 +0300

    9.22

--- patientor/frontend/src/PatientPage/index.tsx
+++ patientor/frontend/src/PatientPage/index.tsx
@@ -1,21 +1,33 @@
 import React from 'react';
-import { PatientPageProps } from '../types';
-import Entry from '../components/Entry';
+import { Gender, PatientPageProps } from '../types';
+import EntryDetails from '../components/EntryDetails';
+import { Icon } from 'semantic-ui-react';
 
 const PatientPage: React.FC<PatientPageProps> = (props) => {
     const patient = props.patient;
 
     console.log(patient);
 
-    if (!patient)
-        return (<p>patient not found</p>);
-    else
+
+    if (!patient) return (<p>patient not found</p>);
+    
+    function getIcon(){
+
+     if (patient.gender === Gender.Male) 
+        return <Icon color="blue" name="male" />;
+     if (patient.gender === Gender.Female) 
+        return <Icon color="pink" name="female" />;
+    return <Icon color="violet" name="transgender alternate" />;
+
+    }
+
         return (<>
-            <h1>{patient.name} </h1>
+            <h1>{patient.name} {getIcon()} 
+            </h1>
             <p> ssn: {patient.ssn} </p>
             <p> occupation: {patient.occupation} </p>
             <h2>entries</h2>
-            { patient.entries.map( e => <Entry key={e.id} entry={e} />) }
+            { patient.entries.map( e => <EntryDetails key={e.id} entry={e} />) }
         </>);
 };
 
---
+++ patientor/frontend/src/components/EntryDetails.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { EntryPropTypes, Entry } from '../types';
+import { useStateValue } from '../state';
+import { Icon, Segment } from 'semantic-ui-react';
+
+const EntryDetails: React.FC<EntryPropTypes> = (props) => {
+    const entry = props.entry;
+
+    const [{ diagnoses },] = useStateValue();
+
+    const assertNever = (value: never): never => {
+        throw new Error(
+            `Unhandled discriminated union member: ${JSON.stringify(value)}`
+        );
+    };
+
+    function getDiagnoseName(code: string): string {
+        if (code) {
+            return diagnoses[code]?.name;
+        }
+        return '';
+    }
+
+    if (!entry) return null;
+
+    function getIcon(entry: Entry) {
+        switch (entry.type) {
+            case 'HealthCheck':
+                return (<Icon size="big" color="green" name="doctor" />);
+            case 'OccupationalHealthcare':
+                return (<Icon size="big" color="blue" name="desktop" />);
+            case 'Hospital':
+                return (<Icon size="big" color="red" name="plus square outline" />);
+            default:
+                assertNever(entry);
+        }
+        return (<Icon name="dot circle" />);
+    }
+
+    return (
+        <Segment>
+            <p> {getIcon(entry)} {entry.description} </p>
+            {
+                entry?.diagnosisCodes
+                && entry.diagnosisCodes.map(code => <li key={code}> {code} {getDiagnoseName(code)}</li>)
+            }
+        </Segment>
+    );
+};
+
+export default EntryDetails;
